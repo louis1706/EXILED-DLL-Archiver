a8ab9ca92a1f4e99855a2d2131305d6c7b6af5c9
Yamato <louismonneyron5@yahoo.com>
2024-01-29 16:06:14 +0100

    Exiled.CustomModules

--- EXILED-DLL-Archiver/Program.cs
+++ EXILED-DLL-Archiver/Program.cs
@@ -24,7 +24,7 @@ namespace EXILED_DLL_Archiver
             string nw_plugin_path = Path.Combine(path, "SCP Secret Laboratory", "PluginAPI", "plugins", "global");
             string nw_plugin_deps_path = Path.Combine(nw_plugin_path, "dependencies");
 
-            List<string> plugins = new List<string> { "Exiled.CreditTags", "Exiled.CustomItems", "Exiled.CustomRoles", "Exiled.Events", "Exiled.Permissions", };
+            List<string> plugins = new List<string> { "Exiled.CreditTags", "Exiled.CustomModules", "Exiled.Events", "Exiled.Permissions", };
             List<string> pluginsDep = new List<string> { "0Harmony", "Exiled.API", "SemanticVersioning", "System.ComponentModel.DataAnnotations" };
             List<string> nwDep = new List<string> { "Exiled.API", "SemanticVersioning", "Mono.Posix" };
 
